4199b5fe81074e75b38e4733c7b0d6af0f52b221
Robert J. Rouquette <robert.rouquette@focusvq.com>
2020-12-07 20:32:42 -0600

    updating code for new board

--- main.c
+++ main.c
@@ -85,32 +85,47 @@ int main(void) {
 }
 
 void initSysClock(void) {
-    // Configure DFLL for 48MHz, calibrated by USB SOF
-    OSC.DFLLCTRL = OSC_RC32MCREF_USBSOF_gc;
-    NVM.CMD  = NVM_CMD_READ_CALIB_ROW_gc;
-    DFLLRC32M.CALB = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, USBRCOSC));
-    DFLLRC32M.CALA = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, USBRCOSCA));
-    NVM.CMD = 0x00;
+    // enable external 32 kHz crystal
+    OSC.XOSCCTRL = OSC_XOSCSEL_32KHz_gc;
+
+    CCP = CCP_IOREG_gc; // Security Signature to modify clock
+    OSC.CTRL = OSC_XOSCEN_bm; // enable external 32kHz crystal
+    while(!(OSC.STATUS & OSC_XOSCRDY_bm)); // wait for oscillator ready
+
+    // Configure DFLL for 48MHz and 2MHz using external crystal
+    // DFLL for USB
     DFLLRC32M.COMP1 = 0x1B; //Xmega AU manual, 4.17.19
     DFLLRC32M.COMP2 = 0xB7;
     DFLLRC32M.CTRL = DFLL_ENABLE_bm;
+    DFLLRC2M.COMP1 = 0xA1; //Xmega AU manual, 4.17.19
+    DFLLRC2M.COMP2 = 0x07;
+    DFLLRC2M.CTRL = DFLL_ENABLE_bm;
 
-    CCP = CCP_IOREG_gc; //Security Signature to modify clock
-    OSC.CTRL = OSC_RC32MEN_bm | OSC_RC2MEN_bm; // enable internal 32MHz oscillator
+    OSC.DFLLCTRL = OSC_RC32MCREF_XOSC32K_gc | OSC_RC2MCREF_XOSC32K_gc;
+    NVM.CMD  = NVM_CMD_READ_CALIB_ROW_gc;
+    DFLLRC32M.CALB = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, USBRCOSC));
+    DFLLRC32M.CALA = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, USBRCOSCA));
+    DFLLRC2M.CALB = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, RCOSC2M));
+    DFLLRC2M.CALA = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, RCOSC2MA));
+    NVM.CMD = 0x00;
+
+    CCP = CCP_IOREG_gc; // Security Signature to modify clock
+    OSC.CTRL = OSC_RC32MEN_bm | OSC_RC2MEN_bm | OSC_XOSCEN_bm; // enable internal 32MHz oscillator
 
     while(!(OSC.STATUS & OSC_RC32MRDY_bm)); // wait for oscillator ready
     while(!(OSC.STATUS & OSC_RC2MRDY_bm)); // wait for oscillator ready
 
+    // enable pll
     OSC.PLLCTRL = OSC_PLLSRC_RC2M_gc | 16u; // 2MHz * 16 = 32MHz
 
     CCP = CCP_IOREG_gc;
-    OSC.CTRL = OSC_RC32MEN_bm | OSC_PLLEN_bm | OSC_RC2MEN_bm ; // Enable PLL
+    OSC.CTRL = OSC_PLLEN_bm | OSC_RC32MEN_bm | OSC_RC2MEN_bm | OSC_XOSCEN_bm; // Enable PLL
 
     while(!(OSC.STATUS & OSC_PLLRDY_bm)); // wait for PLL ready
 
     DFLLRC2M.CTRL = DFLL_ENABLE_bm;
 
-    CCP = CCP_IOREG_gc; //Security Signature to modify clock
+    CCP = CCP_IOREG_gc; // Security Signature to modify clock
     CLK.CTRL = CLK_SCLKSEL_PLL_gc; // Select PLL
     CLK.PSCTRL = CLK_PSADIV_1_gc | CLK_PSBCDIV_1_1_gc; // CPU 32 MHz
 }
--- regulator.c
+++ regulator.c
@@ -12,7 +12,7 @@
 
 #define COMPARATOR_PORT PORTA
 #define COMPARATOR_OUT (0xc0u)
-#define COMPARATOR_IN (0x03u)
+#define COMPARATOR_IN (0x18u)
 
 void initRegulator() {
     // configure regulator enable pin
@@ -27,11 +27,11 @@ void initRegulator() {
 
     // configure analog comparators
     COMPARATOR_PORT.DIRCLR = COMPARATOR_IN;
-    COMPARATOR_PORT.PIN0CTRL = 0x07u;
-    COMPARATOR_PORT.PIN1CTRL = 0x07u;
+    COMPARATOR_PORT.PIN3CTRL = 0x07u;
+    COMPARATOR_PORT.PIN4CTRL = 0x07u;
 
-    ACA.AC0MUXCTRL = 0x05u; // pin 0 vs DAC0
-    ACA.AC1MUXCTRL = 0x0fu; // pin 1 vs Vcc Scaler
+    ACA.AC0MUXCTRL = AC_MUXPOS_PIN3_gc | AC_MUXNEG_DAC_gc; // pin PA3 vs DAC0 (internal)
+    ACA.AC1MUXCTRL = AC_MUXPOS_PIN4_gc | AC_MUXNEG_PIN5_gc; // pin PA4 vs DAC1 (pin PA5)
     ACA.CTRLA = 0x03u;      // enable comparator outputs
     ACA.AC0CTRL = 0x01u;    // enable comparator 0
     ACA.AC1CTRL = 0x01u;    // enable comparator 1
@@ -40,17 +40,19 @@ void initRegulator() {
     NVM.CMD  = NVM_CMD_READ_CALIB_ROW_gc;
     DACB.CH0OFFSETCAL = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, DACB0OFFCAL));
     DACB.CH0GAINCAL   = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, DACB0GAINCAL));
+    DACB.CH1OFFSETCAL = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, DACB1OFFCAL));
+    DACB.CH1GAINCAL   = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, DACB1GAINCAL));
     NVM.CMD = 0x00;
-    DACB.CTRLC = DAC_REFSEL_AVCC_gc;
+    DACB.CTRLC = DAC_REFSEL_AREFA_gc; // Vcc (3.3V)
     DACB.CTRLB = DAC_CHSEL_SINGLE_gc;
-    DACB.CTRLA = DAC_IDOEN_bm | DAC_CH0EN_bm | DAC_ENABLE_bm;
+    DACB.CTRLA = DAC_IDOEN_bm | DAC_CH0EN_bm| DAC_CH1EN_bm | DAC_ENABLE_bm;
 
 
     // configure ADCA
     ADCA.PRESCALER = ADC_PRESCALER_DIV512_gc;
     ADCA.EVCTRL = 0x00;
-    ADCA.REFCTRL = ADC_REFSEL_INTVCC2_gc; // Vcc / 2
-    ADCA.CTRLB = ADC_CONMODE_bm | ADC_RESOLUTION_12BIT_gc; // Signed 12-bit right-adjusted
+    ADCA.REFCTRL = ADC_REFSEL_AREFA_gc; // Vcc (3.3V)
+    ADCA.CTRLB = ADC_RESOLUTION_12BIT_gc; // Unsigned 12-bit right-adjusted
     NVM.CMD  = NVM_CMD_READ_CALIB_ROW_gc;
     ADCA.CALL = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, ADCACAL0));
     ADCA.CALH = pgm_read_byte(offsetof(NVM_PROD_SIGNATURES_t, ADCACAL1));
@@ -58,13 +60,13 @@ void initRegulator() {
 
     // Channel 0 (pin 0)
     ADCA.CH0.INTCTRL = 0x00u; // no interrupt
-    ADCA.CH0.CTRL = ADC_CH_GAIN_DIV2_gc | ADC_CH_INPUTMODE_DIFFWGAIN_gc;
-    ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN0_gc | ADC_CH_MUXNEG_INTGND_MODE4_gc;
+    ADCA.CH0.CTRL = ADC_CH_INPUTMODE_SINGLEENDED_gc;
+    ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN3_gc;
 
     // Channel 1 (pin 1)
     ADCA.CH1.INTCTRL = 0x00u; // no interrupt
-    ADCA.CH1.CTRL = ADC_CH_GAIN_DIV2_gc | ADC_CH_INPUTMODE_DIFFWGAIN_gc;
-    ADCA.CH1.MUXCTRL = ADC_CH_MUXPOS_PIN1_gc | ADC_CH_MUXNEG_INTGND_MODE4_gc;
+    ADCA.CH1.CTRL = ADC_CH_INPUTMODE_SINGLEENDED_gc;
+    ADCA.CH1.MUXCTRL = ADC_CH_MUXPOS_PIN4_gc;
 
     // Enable ADC
     ADCA.CTRLA = 0x01;
@@ -78,46 +80,41 @@ void enableRegulator(bool enabled) {
 }
 
 void setOutputVoltage(uint16_t limit) {
-    // safety check (190.00 V)
-    if(limit > 19000u) limit = 19000u;
+    // safety check (195.00 V)
+    if(limit > 19500u) limit = 19500u;
 
     uint32_t tmp = limit;
-    tmp <<= 6u;
+    tmp *= 4096u;
     tmp += 11750u;
     tmp /= 23500u;
-    uint8_t scalefac = (uint8_t) tmp;
-
-    if(scalefac > 64u) scalefac = 64u;
-    else if(scalefac < 1u) scalefac = 1u;
-    ACA.CTRLB = scalefac - 1u;
+    DACB.CH0DATA = (uint16_t) tmp;
 }
 
 void setInputVoltage(uint16_t limit) {
-    // safety check (100.00 V)
-    if(limit > 10000u) limit = 10000u;
+    // safety check (95.00 V)
+    if(limit > 9500u) limit = 9500u;
 
     uint32_t tmp = limit;
     tmp *= 4096u;
     tmp += 5431u;
     tmp /= 10862u;
-    DACB.CH0DATA = (uint16_t) tmp;
+    DACB.CH1DATA = (uint16_t) tmp;
 }
 
 uint16_t getOutputVoltage() {
-    uint32_t tmp = ACA.CTRLB & 0x3fu;
-    tmp += 1u;
+    uint32_t tmp = DACB.CH0DATA & 0xfffu;
     tmp *= 23500u;
-    tmp += 32u;
-    tmp /= 64u;
+    tmp += 2048u;
+    tmp /= 4096u;
     return (uint16_t) tmp;
 }
 
 uint16_t getOutputVoltageStep() {
-    return (23500u + 32u) / 64u;
+    return (10862u + 2048u) / 4096u;
 }
 
 uint16_t getInputVoltage() {
-    uint32_t tmp = DACB.CH0DATA & 0xfffu;
+    uint32_t tmp = DACB.CH1DATA & 0xfffu;
     tmp *= 10862u;
     tmp += 2048u;
     tmp /= 4096u;
@@ -125,32 +122,32 @@ uint16_t getInputVoltage() {
 }
 
 uint16_t getInputVoltageStep() {
-    return (10862u + 2047u) / 4095u;
+    return (10862u + 2048u) / 4096u;
 }
 
 uint16_t measureOutputVoltage() {
-    ADCA.CH1.CTRL |= ADC_CH_START_bm;
-    while(!(ADCA.CH1.INTFLAGS & 0x01u));
-    ADCA.CH1.INTFLAGS = 0x01u;
-
-    uint32_t tmp = ADCA.CH1.RES & 0xfffu;
-    if(tmp & 0x800u) tmp = 0;
-    tmp *= 23500u;
-    tmp += 1023u;
-    tmp /= 2047u;
-    return (uint16_t) tmp;
-}
-
-uint16_t measureInputVoltage() {
     ADCA.CH0.CTRL |= ADC_CH_START_bm;
     while(!(ADCA.CH0.INTFLAGS & 0x01u));
     ADCA.CH0.INTFLAGS = 0x01u;
 
     uint32_t tmp = ADCA.CH0.RES & 0xfffu;
-    if(tmp & 0x800u) tmp = 0;
+    tmp = (tmp > 200) ? (tmp - 200) : 0;
+    tmp *= 23500u;
+    tmp += 1948u;
+    tmp /= 3896u;
+    return (uint16_t) tmp;
+}
+
+uint16_t measureInputVoltage() {
+    ADCA.CH1.CTRL |= ADC_CH_START_bm;
+    while(!(ADCA.CH1.INTFLAGS & 0x01u));
+    ADCA.CH1.INTFLAGS = 0x01u;
+
+    uint32_t tmp = ADCA.CH1.RES & 0xfffu;
+    tmp = (tmp > 200) ? (tmp - 200) : 0;
     tmp *= 10862u;
-    tmp += 1023u;
-    tmp /= 2047u;
+    tmp += 1948u;
+    tmp /= 3896u;
     return (uint16_t) tmp;
 }
 
